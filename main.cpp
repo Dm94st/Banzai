f53ccdac3ae88577a6d16dacba5bb431ff29cdc7
Dmitry <stavitsky94@gmail.com>
2018-03-06 09:05:23 +0300

    Codestyle fixes

--- main.cpp
+++ main.cpp
@@ -90,17 +90,17 @@ public:
 
     for(auto it = srcArgs.begin(); it != srcArgs.end(); ++it)
     {
-        double curValue = std::stod(*it);
+      double curValue = std::stod(*it);
 
-        // If current value in avialible range
-        if( curValue > lowBound && curValue < hiBound)
-        {
-          double interpValue = InterpolateValue(curValue);
-          m_interpValues.push_back(interpValue);
-          std::cout<<interpValue<<std::endl;
-        }
-        else
-          continue;
+      // If current value in avialible range
+      if( curValue > lowBound && curValue < hiBound)
+      {
+        double interpValue = InterpolateValue(curValue);
+        m_interpValues.push_back(interpValue);
+        std::cout<<interpValue<<std::endl;
+      }
+      else
+        continue;
     }
     CSVReader::WriteInFile(m_outPath, m_interpValues);
   }
@@ -113,21 +113,21 @@ protected:
   // Find nearest index of row
   int IdxOfNearestRow(double x)
   {
-      std::vector<std::string> srcXY = m_srcData.GetData(); // Source data
-      std::vector<double> curValue, nextValue; // Source X and Y of current row
-      int desiredIdx = 0; // Index of row
-      int idx = 0; // Index
+    std::vector<std::string> srcXY = m_srcData.GetData(); // Source data
+    std::vector<double> curValue, nextValue; // Source X and Y of current row
+    int desiredIdx = 0; // Index of row
+    int idx = 0; // Index
 
-      for(auto it = srcXY.begin(); it != srcXY.end() - 1; ++it, ++idx)
+    for(auto it = srcXY.begin(); it != srcXY.end() - 1; ++it, ++idx)
+    {
+      curValue = SplitByDelimiter(*it);
+      nextValue = SplitByDelimiter(*(std::next(it)));
+      if(x >= curValue.front() && x <= nextValue.front())
       {
-        curValue = SplitByDelimiter(*it);
-        nextValue = SplitByDelimiter(*(std::next(it)));
-        if(x >= curValue.front() && x <= nextValue.front())
-        {
-            desiredIdx = std::distance(srcXY.begin(), it);
-        }
+        desiredIdx = std::distance(srcXY.begin(), it);
       }
-      return desiredIdx;
+    }
+    return desiredIdx;
   }
 
   // Split each row by delimiter
@@ -160,6 +160,7 @@ class NearestNeighbor: public Interpolator
 {
 public:
   using Interpolator::Interpolator; // Inherit constructor from base class
+
 private:
   double InterpolateValue(double x) override
   {
@@ -172,143 +173,147 @@ private:
 ;
 class Linear: public Interpolator
 {
-    using Interpolator::Interpolator; // Inherit constructor from base class
+public:
+  using Interpolator::Interpolator;
 
-    double InterpolateValue(double x) override
-    {
-      std::vector<std::string> srcXY = m_srcData.GetData();
-      int desiredInterval = IdxOfNearestRow(x);
+private:
+  double InterpolateValue(double x) override
+  {
+    std::vector<std::string> srcXY = m_srcData.GetData();
+    int desiredInterval = IdxOfNearestRow(x);
 
-      double xVal = SplitByDelimiter(srcXY[desiredInterval]).front(); // X_n
-      double yVal = SplitByDelimiter(srcXY[desiredInterval]).back(); // Y_n
+    double xVal = SplitByDelimiter(srcXY[desiredInterval]).front(); // X_n
+    double yVal = SplitByDelimiter(srcXY[desiredInterval]).back(); // Y_n
 
-      double xValNext = SplitByDelimiter(srcXY[desiredInterval + 1]).front(); // X_n+1
-      double yValNext = SplitByDelimiter(srcXY[desiredInterval + 1]).back(); // Y_n+1
+    double xValNext = SplitByDelimiter(srcXY[desiredInterval + 1]).front(); // X_n+1
+    double yValNext = SplitByDelimiter(srcXY[desiredInterval + 1]).back(); // Y_n+1
 
-      double interpolatedValue = yVal + (yValNext - yVal) * (x - xVal)/(xValNext - xVal);
-      return interpolatedValue;
-    }
+    double interpolatedValue = yVal + (yValNext - yVal) * (x - xVal)/(xValNext - xVal);
+    return interpolatedValue;
+  }
 }
 ;
 class Quadratic: public Interpolator
 {
-    using Interpolator::Interpolator;
+public:
+  using Interpolator::Interpolator;
 
-    double InterpolateValue(double x) override
-    {
-      /*
-      Formula:
-      y = y1*(x-x2)*(x-x3)/(x1-x2)(x1-x3) +
-      y2*(x-x1)*(x-x3)/(x2-x1)*(x2-x3) +
-      y3*(x-x1)*(x-x2)/(x3-x1)*(x3-x2)
-      */
+private:
+  double InterpolateValue(double x) override
+  {
+    /*
+    Formula:
+    y = y1*(x-x2)*(x-x3)/(x1-x2)(x1-x3) +
+    y2*(x-x1)*(x-x3)/(x2-x1)*(x2-x3) +
+    y3*(x-x1)*(x-x2)/(x3-x1)*(x3-x2)
+    */
 
-      std::vector<std::string> srcXY = m_srcData.GetData();
-      int desiredIdx = IdxOfNearestRow(x);
+    std::vector<std::string> srcXY = m_srcData.GetData();
+    int desiredIdx = IdxOfNearestRow(x);
 
-      double xValPrev = SplitByDelimiter(srcXY[desiredIdx - 1]).front(); // X_n-1
-      double yValPrev = SplitByDelimiter(srcXY[desiredIdx - 1]).back(); // Y_n-1
+    double xValPrev = SplitByDelimiter(srcXY[desiredIdx - 1]).front(); // X_n-1
+    double yValPrev = SplitByDelimiter(srcXY[desiredIdx - 1]).back(); // Y_n-1
 
-      double xVal = SplitByDelimiter(srcXY[desiredIdx]).front(); // X_n
-      double yVal = SplitByDelimiter(srcXY[desiredIdx]).back(); // Y_n
+    double xVal = SplitByDelimiter(srcXY[desiredIdx]).front(); // X_n
+    double yVal = SplitByDelimiter(srcXY[desiredIdx]).back(); // Y_n
 
-      double xValNext = SplitByDelimiter(srcXY[desiredIdx + 1]).front(); // X_n+1
-      double yValNext = SplitByDelimiter(srcXY[desiredIdx + 1]).back(); // Y_n+1
+    double xValNext = SplitByDelimiter(srcXY[desiredIdx + 1]).front(); // X_n+1
+    double yValNext = SplitByDelimiter(srcXY[desiredIdx + 1]).back(); // Y_n+1
 
-      double interpolatedValue = (yValPrev*((x-xVal)*(x-xValNext)))/((xValPrev-xVal)*(xValPrev-xValNext)) +\
-                                 (yVal*((x-xValPrev)*(x-xValNext)))/((xVal-xValPrev)*(xVal-xValNext)) +\
-                                 (yValNext*((x-xValPrev)*(x-xVal)))/((xValNext-xValPrev)*(xValNext-xVal));
-      return interpolatedValue;
-    }
+    double interpolatedValue = (yValPrev*((x-xVal)*(x-xValNext)))/((xValPrev-xVal)*(xValPrev-xValNext)) +\
+                               (yVal*((x-xValPrev)*(x-xValNext)))/((xVal-xValPrev)*(xVal-xValNext)) +\
+                               (yValNext*((x-xValPrev)*(x-xVal)))/((xValNext-xValPrev)*(xValNext-xVal));
+    return interpolatedValue;
+  }
 }
 ;
 
 enum InterpType
 {
-    NEAREST_NEIGHBOR = 1,
-    LINEAR = 2,
-    QUADRATIC = 3,
+  NEAREST_NEIGHBOR = 1,
+  LINEAR = 2,
+  QUADRATIC = 3,
 };
 
 int main(int argc, char* argv[])
 {
-    std::string srcXYPath;
-    char * delimiter;
-    std::string srcArgsPath;
-    std::string outFilePath;
+  std::string srcXYPath;
+  char * delimiter;
+  std::string srcArgsPath;
+  std::string outFilePath;
 
-    int interpolateMethod = 0;
+  int interpolateMethod = 0;
 
-    //Except four arguments
-    if (argc < 4)
+  //Except four arguments
+  if (argc < 4)
+  {
+    std::cerr << "Usage: " << argv[0] << "<Input file with X and Y values> <Input file with args> <Output File>"
+              << std::endl;
+    return 1;
+  }
+  else
+  {
+    for(int i = 1; i < argc; ++i)
     {
-      std::cerr << "Usage: " << argv[0] << "<Input file with X and Y values> <Input file with args> <Output File> <Interpolate method>"
-                << std::endl;
-      return 1;
+      srcXYPath = argv[i++]; // srcValues
+      delimiter = argv[i++]; //delim
+      srcArgsPath = argv[i++]; // srcArgs
+      outFilePath = argv[i++]; // Out Path
     }
-    else
+  }
+
+  CSVReader srcXY(srcXYPath, *delimiter);
+  CSVReader srcArgs(srcArgsPath);
+
+  std::cout<<"srcXYPath: "  <<srcXYPath<<"\n" \
+           <<"delim: "      <<delimiter<<"\n" \
+           <<"srcArgsPath: "<<srcArgsPath<<"\n" \
+           <<"outFilePath: "<<outFilePath<<"\n" \
+           <<std::endl;
+
+  bool isRunning = true;
+
+  while(isRunning)
+  {
+    std::cout<<"Enter interpolate method"<<std::endl;
+    std::cin >> interpolateMethod;
+    try
     {
-      for(int i = 1; i < argc; ++i)
+      switch(interpolateMethod)
       {
-        srcXYPath = argv[i++]; // srcValues
-        delimiter = argv[i++]; //delim
-        srcArgsPath = argv[i++]; // srcArgs
-        outFilePath = argv[i++]; // Out Path
-      }
-    }
-
-    CSVReader srcXY(srcXYPath, *delimiter);
-    CSVReader srcArgs(srcArgsPath);
-
-    std::cout<<"srcXYPath: " <<srcXYPath<<"\n" \
-             <<"delim: " <<delimiter<<"\n" \
-             <<"srcArgsPath: "<<srcArgsPath<<"\n" \
-             <<"outFilePath: "<<outFilePath<<"\n" \
-             <<std::endl;
-
-    bool isRunning = true;
-
-    while(isRunning)
-    {
-      std::cout<<"Enter interpolate method"<<std::endl;
-      std::cin >> interpolateMethod;
-      try
-      {
-        switch(interpolateMethod)
+        case InterpType::LINEAR:
         {
-          case InterpType::LINEAR:
-          {
-            std::cout<<"You are using linear method"<<std::endl;
-            auto linearMethod = std::make_shared<Linear>(srcXY,srcArgs,outFilePath);
-            linearMethod->InterpolateAll();
-            break;
-          }
-          case InterpType::NEAREST_NEIGHBOR:
-          {
-            std::cout<<"You are using Nearest Neighbor method"<<std::endl;
-            auto nnMethod = std::make_shared<NearestNeighbor>(srcXY,srcArgs,outFilePath);
-            nnMethod->InterpolateAll();
-            break;
-          }
-          case InterpType::QUADRATIC:
-          {
-            std::cout<<"You are using quadratic method"<<std::endl;
-            auto quadMethod = std::make_shared<Quadratic>(srcXY,srcArgs,outFilePath);
-            quadMethod->InterpolateAll();
-            break;
-          }
-          default:
-          {
-            isRunning = false;
-            throw std::invalid_argument("Unknown method!");
-            break;
-          }
+          std::cout<<"You are using linear method"<<std::endl;
+          auto linearMethod = std::make_shared<Linear>(srcXY,srcArgs,outFilePath);
+          linearMethod->InterpolateAll();
+          break;
+        }
+        case InterpType::NEAREST_NEIGHBOR:
+        {
+          std::cout<<"You are using Nearest Neighbor method"<<std::endl;
+          auto nnMethod = std::make_shared<NearestNeighbor>(srcXY,srcArgs,outFilePath);
+          nnMethod->InterpolateAll();
+          break;
+        }
+        case InterpType::QUADRATIC:
+        {
+          std::cout<<"You are using quadratic method"<<std::endl;
+          auto quadMethod = std::make_shared<Quadratic>(srcXY,srcArgs,outFilePath);
+          quadMethod->InterpolateAll();
+          break;
+        }
+        default:
+        {
+          isRunning = false;
+          throw std::invalid_argument("Unknown method!");
+          break;
         }
       }
-      catch(std::exception const &ex)
-      {
-        std::cout<<ex.what()<<std::endl;
-      }
     }
-    return 0;
+    catch(std::exception const &ex)
+    {
+      std::cout<<ex.what()<<std::endl;
+    }
+  }
+  return 0;
 }
